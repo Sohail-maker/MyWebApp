dee68f6fc81427f3f317c3ba2b4bb65bd261fac5
Sohail Amiri - Office <sms.sohail@hotmail.com>
2021-06-12 16:27:27 +0430

    added login in master page

--- MyWebApp/WebApp.Master.cs
+++ MyWebApp/WebApp.Master.cs
@@ -13,5 +13,10 @@ namespace MyWebApp
         {
           
         }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- MyWebApp/WebApp.Master.designer.cs
+++ MyWebApp/WebApp.Master.designer.cs
@@ -33,6 +33,33 @@ namespace MyWebApp
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// inputUser control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText inputUser;
+
+        /// <summary>
+        /// inputPass control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputPassword inputPass;
+
+        /// <summary>
+        /// btnLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogin;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
